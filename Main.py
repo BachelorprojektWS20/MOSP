f73d1d27f168e8563bf0715c8923eb85ae136743
Friedrich Werner <WernerFriedrich@protonmail.com>
2020-12-20 20:23:11 +0100

    Forgot comments in main.

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 from Motorsteuerung.MotorControl import MotorControl
 import time
 
+''' Hauptfunktion welche die Motorsteuerung sowie alle dazu gehörenden Komponenten started.
+'''
 if __name__ == "__main__":
     #TODO: Andere PINs
+
+    # Pin Nummerierung für die Steuerung der Motoren. Verwendete Numerierung ist das BCM System.
     VL = (2, 3, 4)
     VR = (17, 27, 22)
     HL = (23, 24, 25)
